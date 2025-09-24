0b33a57fb51dc16c2abe87fd9695fa6aea4105ca
britodeandrade89 <britodeandrade@gmail.com>
2025-09-24 09:59:10 -0300

    script.js

--- Script.js
+++ Script.js
@@ -12,3 +12,29 @@ document.addEventListener("DOMContentLoaded", () => {
 function abrirPerfil(nome) {
   window.location.href = `perfil.html?aluno=${encodeURIComponent(nome)}`;
 }
+let alunoSelecionado = "";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const params = new URLSearchParams(window.location.search);
+  alunoSelecionado = params.get("aluno");
+
+  const titulo = document.getElementById("titulo-perfil");
+  if (alunoSelecionado && titulo) {
+    titulo.textContent = `Perfil de ${alunoSelecionado}`;
+  }
+});
+
+function abrirTreino(arquivo) {
+  fetch(arquivo)
+    .then(res => res.text())
+    .then(texto => {
+      document.getElementById("conteudo-arquivo").textContent = texto;
+    })
+    .catch(err => {
+      document.getElementById("conteudo-arquivo").textContent = "Erro ao carregar arquivo: " + err;
+    });
+}
+
+function voltar() {
+  window.location.href = "index.html";
+}
