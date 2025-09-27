cf95389f663bd9a150c6f27c1f08bc76bd7e582f
britodeandrade89 <britodeandrade@gmail.com>
2025-09-27 01:49:55 -0300

    Create armazenamento-local.js

---
+++ armazenamento-local.js
@@ -0,0 +1,5 @@
+// Chaves utilizadas no localStorage
+abfit_database          # Dados principais
+abfit_pending_sync      # Sincronização pendente  
+abfit_outdoor_workouts  # Atividades outdoor
+abfit_physio_alunos     # Avaliações físicas
